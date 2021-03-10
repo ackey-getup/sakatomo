34c3e075d9014eb9339fdee3d577bfebbfdefab1
ackey-getup <akira.getup@gmail.com>
2021-03-10 16:35:18 +0900

    ssh登録

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,8 @@
 server '3.130.10.219', user: 'ec2-user', roles: %w{app db web}
+
+# CircleCIのGUIで設定した環境変数を使ってSSH接続
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
